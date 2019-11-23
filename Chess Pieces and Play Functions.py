f0ed8e85f307cc872b976d4cbd73dc910337d2bb
Varigarble <58060789+Varigarble@users.noreply.github.com>
2019-11-22 17:00:42 -0800

    added files
    Chessboard.py
    Chess Pieces and Play Functions.py

---
+++ Chess Pieces and Play Functions.py
@@ -0,0 +1,184 @@
+''' This is a library of standard chess pieces and functions showing the possible moves of each.'''
+
+
+''' Piece function section '''
+
+
+def white_pawn():
+    # TODO: check for valid inputs, rewrite to absolute cell values
+    global position
+
+    attacked = {None}
+    capture = {None}
+    promotion = None
+    invalid = None
+
+    if (56 < position < 65):
+        print("You must promote to a new queen, rook, bishop or knight")
+        return promotion
+    if (0 < position < 9):
+        print("Pawns can't be there")
+        return invalid
+    if (8 < position < 57):
+        attacked.add(position + 1)
+        if ((position + 7) % 8) != 0:
+            capture.add(position + 7)
+        if ((position + 8) % 8) != 0:
+            capture.add(position + 9)
+    if (8 < position < 17):
+        attacked.add(position + 2)
+
+    return attacked, capture
+# attacked, capture = white_pawn()
+# print(attacked)
+# print(capture)
+
+def black_pawn():
+    # TODO: check for valid inputs, rewrite to absolute cell values
+    global position
+
+    attacked = {None}
+    capture = {None}
+    promotion = None  # placeholder for some other possiblity
+    invalid = None  # placeholder for some other possiblity
+
+    if (0 < position < 9):
+        print("You must promote to a new queen, rook, bishop or knight")
+        return promotion
+    if (56 < position < 65):
+        print("Pawns can't be there")
+        return invalid
+    if (8 < position < 57):
+        attacked.add(position - 1)
+        if ((position - 8) % 8) != 0:
+            capture.add(position - 7)
+        if ((position - 9) % 8) != 0:
+            capture.add(position - 9)
+    if (48 < position < 57):
+        attacked.add(position - 2)
+
+    return attacked, capture
+# attacked, capture = white_pawn()
+# print(attacked)
+# print(capture)
+
+def rook():
+    # TODO: check for valid inputs; see if advantage to lists instead of sets; combine row, col?
+    global position
+
+    attacked = {None}
+    # row
+    while (position > 1) and (position - 1) % 8 != 0:
+        attacked.add(position - 1)
+        position -= 1
+    position = reset_position
+
+    while (position < 64) and (position % 8 != 0):
+        attacked.add(position + 1)
+        position += 1
+    # reset_position = position <- set at beginning
+    position = reset_position
+    # column
+    while ((position - 8) > 0) and (position > 8):
+        attacked.add(position - 8)
+        position -= 8
+    position = reset_position
+    while (position + 8) < 65:
+        attacked.add(position + 8)
+        position += 8
+    position = reset_position
+    return attacked
+
+
+def bishop():
+    # TODO: check for valid inputs; see if advantage to lists instead of sets; combine row, col?
+    global position
+
+    attacked = {None}
+
+    # ll to ur diagonal
+    while (position > 1) and (position < 65) and (((position - 1) % 8) != 0):
+        if (position - 9) > 0:
+            attacked.add(position - 9)
+        position -= 9
+    position = reset_position
+    while (position > 0) and (position < 64) and ((position % 8) != 0):
+        if (position + 9) < 65 and ((position + 9) > position % 8):
+            attacked.add(position + 9)
+        position += 9
+    position = reset_position
+
+    # ul to lr diagonal
+    while (position > 1) and (position < 64) and ((position % 8) != 0):
+        if (position - 7) > 0:
+            attacked.add(position - 7)
+        position -= 7
+    position = reset_position
+    while (position > 1) and (position < 64) and (((position + 7) % 8) != 0):
+        if (position + 7) < 65:
+            attacked.add(position + 7)
+        position += 7
+    position = reset_position
+    return attacked
+
+
+def queen():
+    attacked = rook() | bishop()
+    return attacked
+
+
+def knight():
+    global position
+    attacked = {None}
+
+    # vertical up & left/right
+    if (((position + 15) % 8) != 0) and (position + 15 < 64):
+        attacked.add(position + 15)
+    position = reset_position
+    if (((position + 16) % 8) != 0) and (position + 17 < 64):
+        attacked.add(position + 17)
+    position = reset_position
+
+    # horizontal left & up/down
+    if (((position - 1) % 8) != 0) and (((position - 2) % 8) != 0) \
+            and (2 < position < 57):
+        attacked.add(position + 6)
+    position = reset_position
+    if (((position - 1) % 8) != 0) and (((position - 2) % 8) != 0) \
+            and (10 < position < 65):
+        attacked.add(position - 10)
+    position = reset_position
+
+    # horizontal right & up/down
+    if ((position % 8) != 0) and (((position + 1) % 8) != 0) \
+            and (position < 55):
+        attacked.add(position + 10)
+    position = reset_position
+    if ((position % 8) != 0) and (((position + 1)) % 8 != 0) \
+            and (8 < position < 63):
+        attacked.add(position - 6)
+    position = reset_position
+
+    # vertical down & left/right
+    if (((position - 17) % 8) != 0) and (position - 17 > 0):
+        attacked.add(position - 17)
+    position = reset_position
+    if (((position - 16) % 8) != 0) and (position - 15 > 1):
+        attacked.add(position - 15)
+    position = reset_position
+
+    return attacked
+
+def white_king():
+    global position
+    attacked = {None}
+
+    if position == 61:
+        attacked.add(63)
+    # upper row
+    for x in range(position + 7, position + 9):
+        if (position < 57) and (((position + 7) % 8) != 0) and (((position + 7) % 8) != 0):
+            attacked.add(x)
+
+
+    return attacked
---
+++ Chessboard.py
@@ -0,0 +1,99 @@
+import string
+# https://stackoverflow.com/questions/16060899/alphabet-range-on-python
+''' Chessboard is absolute positions of 1-64'''
+''' Piece input section 
+TODO: try/except for ValueError for invalid input; 
+'''
+
+piece = input("State your piece: ")
+
+p = piece.upper()
+
+
+def piece_name_display():
+    if p[0] not in {"P", "R", "N", "KN", "H", "K", "KI", "B", "Q"}:
+        raise ValueError
+
+    if p[0] == "P":
+        print("Your piece is a pawn")
+
+    if p[0] == "R":
+        print("Your piece is a rook")
+
+    if p[0] == "N" or p[:1] == "KN":
+        print("Your piece is a knight")
+
+    if p[0] == "H":
+        print("Your piece is a knight, NOT a horse")
+
+    if p[0] == "K" or p[:1] == "KI":
+        print("Your piece is a king")
+
+    if p[0] == "B":
+        print("Your piece is a bishop")
+
+    if p[0] == "Q":
+        print("Your piece is a queen")
+
+
+piece_name_display()
+
+color = input("State your color: ").upper()
+
+
+def piece_color_display():
+    if color[0] == "B":
+        print("Your piece is black")
+
+    if color[0] == "W":
+        print("Your piece is white")
+
+
+piece_color_display()
+
+
+def locator():
+    # gets chess notation input and converts to absolute cell values
+    row = int(input("State your row (1-8): "))
+    raw_column = input("State your column (A-H): ").upper()
+    column = (list(string.ascii_uppercase).index(raw_column)) + 1
+    position = (row - 1) * 8 + column
+    return position
+
+
+locator()
+
+reset_position = position
+# sample board inputs from piece function outputs:
+attacked = {1, 34, 35, 36, 37, 38, 39, 40, 9, 41, 17, 49, 25, 57}
+capture = {9, 11}
+raw_piece = "rook"
+piece = raw_piece[0].upper()
+position = 33
+
+# draw board using array contents
+row = 8
+print("", " -" * 8)
+
+print(row, "|", sep="", end="")
+
+for all_squares in range(8, 0, -1):
+    # global row
+    for square in range((8 * (row - 1) + 1), (8 * (row - 1) + 9)):
+        if square == position:
+            print(piece, "|", sep="", end="")
+        elif square not in attacked: # add not in capture?
+            print(" ", "|", sep="", end="")
+        elif square in attacked:
+            print("X", "|", sep="", end="")
+        elif square in capture:
+            print("C", "|", sep="", end="")
+    row -= 1
+    print("\n", " -" * 8)
+    if row > 0:
+        print(row, "|", sep="", end="")
+    if row == 0:
+        print(" ", end="")
+        for ltr in string.ascii_uppercase[0:8]:
+            print("", ltr, end="")
+        print("\n")
